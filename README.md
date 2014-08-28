5e55256816f2c071070946651f79ff9621199fc3
Ell <anton.komarev@buongiorno.com>
2014-08-29 02:29:01 +0400

    Cleanup timer. Removed rest. Removed popups.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Time is Money (TiM)
 ===================
 
-Tool for people who value their time and the time of others.
+Tool for the people who value their time and the time of others.
--- script.js
+++ script.js
@@ -8,14 +8,3 @@ function isLocalStorageAvailable() {
 function wipeLocalStorage() {
     if (isLocalStorageAvailable()) localStorage.clear();
 }
-
-function visualizePlayingState(state) {
-    if (! state) {
-        $('#play').html('<i class="fa fa-pause fa-fw fa-lg"></i>Pause');
-        isWorking = true;
-    }
-    else {
-        $('#play').html('<i class="fa fa-play fa-fw fa-lg"></i>Play');
-        isWorking = false;
-    }
-}
